af6c26e3a5372aad5f72912776399b8b613ec246
Giovanni Klein Campigoto <giovannikleincampigoto@gmail.com>
2020-09-09 07:55:26 -0300

    [*] Create database migrations

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,34 @@
+import {
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Category from './Category';
+
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
